b3091746aa1a263b7fb2bf8356a8a9a7dd0bae6e
IbrahimHQ <ibrahimhqureshi@gmail.com>
2022-12-20 18:32:28 -0800

    fixed checkout styling

--- src/components/payment-form/payment-form.styles.jsx
+++ src/components/payment-form/payment-form.styles.jsx
@@ -11,7 +11,7 @@ export const PaymentFormContainer = styled.div`
 
 export const FormContainer = styled.form`
     height: 100px;
-    min-width: 500 px;
+    min-width: 500px;
 `;
 
 export const PaymentButton = styled(Button)`
